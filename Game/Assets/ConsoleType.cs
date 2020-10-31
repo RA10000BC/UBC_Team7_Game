fd88c073d57782579c525753ad8b93a6e557c7ed
Alex <37099150+RA10000BC@users.noreply.github.com>
2020-10-31 16:22:53 -0700

    new test

--- Game/Assets/ConsoleType.cs
+++ Game/Assets/ConsoleType.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ConsoleType : MonoBehaviour
 {
+
+    [SerializeField] float someNumber;
+    
     // Start is called before the first frame update
     void Start()
     {
